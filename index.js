2bdebca5fd6cfcdb6ebc9371b285f55652e84a5a
Harsh <126139439+palhrsh09@users.noreply.github.com>
2025-06-27 21:32:36 +0530

    added space

--- index.js
+++ index.js
@@ -20,5 +20,6 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/history', historyRoutes);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
